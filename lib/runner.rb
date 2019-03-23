8c370e8e5019e3eed44eef62e07e760d8d0423d4
Christopher K Swofford <christopher.swofford@gmail.com>
2019-03-23 17:05:35 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,9 @@
-require_relative "blackjack.rb"
-
-runner
+describe "#display_card_total" do
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
+  end
+ 
+  it "prints the value of the cards to the screen" do
+    expect($stdout).to receive(:puts).with("Your cards add up to 8")
+    display_card_total(8)
+  end
